1079b4a7a6a322f280bd8ca4868ba1c12dfbd021
Nathan <nate.gjerd@icloud.com>
2019-10-15 21:59:26 -0500

    added splash template, js, and scss for example

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 
 <!-- EXAMPLE OF USING TEMPLATED PHP FILES -->
 <?php include "templates/header.php"; ?>
+<?php include "templates/splash.php"; ?>
 
 <div class="grid-wrapper one-whole">
 	<div class="grid desk-one-quarter">
