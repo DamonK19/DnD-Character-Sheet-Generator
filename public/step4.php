66f9c7fa9e7d3af72a02fea61d29d290ec3874fe
Ian Gormley <50596230+hj7852he@users.noreply.github.com>
2019-10-27 14:55:06 -0500

    Update step4.php

--- public/step4.php
+++ public/step4.php
@@ -5,14 +5,15 @@
     <div class="grid one-whole">
 
         <form action="step5.php" method="post">
-            <input type="checkbox" name="skill1" value="Background1" checked> Background skill
-            <input type="checkbox" name="skill2" value="skill2"> skill 2
-            <input type="checkbox" name="skill3" value="skill3"> skill3
 
+            <div class="checkbox"><label for="checkbox1"><input type="checkbox" checked="checked" name="checkbox" id="checkbox1"> Background skill</label></div>
+            <div class="checkbox"><label for="checkbox2"><input type="checkbox" name="checkbox" id="checkbox2"> Skill A</label></div>
+            <div class="checkbox"><label for="checkbox3"><input type="checkbox" name="checkbox" id="checkbox3"> Skill B</label></div>
+            <div class="checkbox"><label for="checkbox4"><input type="checkbox" name="checkbox" id="checkbox4" disabled> Choice Disabled</label></div>
+            <div class="checkbox has-error"><label for="checkbox5"><input type="checkbox" name="checkbox" id="checkbox5"> Choice Error</label></div>
+            <div class="checkbox has-success"><label for="checkbox6"><input type="checkbox" name="checkbox" id="checkbox6"> Choice Valid</label></div>
             <div class="grid one-whole">
-                <button type="submit" name="button">
-                    submit
-                </button>
+            <p><button class="btn" type="submit">Submit</button></p>
             </div>
         </form>
     </div>
