96bf749774a172a1f06bcc2848f147a8cbfe1c5b
Ian Gormley <50596230+hj7852he@users.noreply.github.com>
2019-10-27 14:30:03 -0500

    w

--- public/step4.php
+++ public/step4.php
@@ -1,11 +1,24 @@
-<?php require "templates/header.php"; ?>
+<!DOCTYPE html>
+<html>
+    <?php require "templates/header.php"; ?>
 <?php require "templates/entitiyselect.php"; ?>
-<div class="grid-wrapper one-whole centered">
+    <body>
+    <div class="grid-wrapper one-whole centered">
     <div class="grid one-whole">
-        blah
-        <button type="button" name="button">
-            "title"
-        </button>
+
+        <form action="step5.php" method="post">
+            <input type="checkbox" name="skill1" value="Background1" checked> Background skill
+            <input type="checkbox" name="skill2" value="skill2"> skill 2
+            <input type="checkbox" name="skill3" value="skill3"> skill3
+
+            <div class="grid one-whole">
+                <button type="submit" name="button">
+                    submit
+                </button>
+            </div>
+        </form>
+    </div>
 </div>
-</div>
+</body>
 <?php require "templates/footer.php"; ?>
+</html>
