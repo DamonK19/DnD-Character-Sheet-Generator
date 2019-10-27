d7572fa913fc17608c7ca767f59ca9dafc72cb82
Nathan <nate.gjerd@icloud.com>
2019-10-27 15:30:27 -0500

    added homepage

---
+++ public/homepage.php
@@ -0,0 +1,12 @@
+
+<?php include "templates/header.php"; ?>
+<div class="logo">
+  <img src="assets/images/logo.png" alt="">
+</div>
+<div class="slideshows">
+  <div class="slide bg-1"></div>
+  <div class="slide bg-2"></div>
+  <div class="slide bg-3"></div>
+</div>
+
+<?php include "templates/footer.php"; ?>
--- public/templates/footer.php
+++ public/templates/footer.php
@@ -1,6 +1,7 @@
 <!-- SCRIPTS GO HERE -->
 </div>
 <script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="assets/js/homepage.js"></script>
 <script type="text/javascript" src="assets/js/dice.js"></script>
 <script type="text/javascript" src="assets/js/removeSmallestFromArray.js"></script>
 <script type="text/javascript" src="assets/js/ability_score_roller.js"></script>
