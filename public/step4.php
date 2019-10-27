e85b2a838de513ef9b0e741c5117376012125b4b
Ian Gormley <50596230+hj7852he@users.noreply.github.com>
2019-10-27 15:20:01 -0500

    Update step4.php

--- public/step4.php
+++ public/step4.php
@@ -1,17 +1,15 @@
 <?php require "templates/header.php"; ?>
-<?php require "templates/entitiyselect.php"; ?>
-
 <div class="grid-wrapper one-whole centered">
     <div class="grid one-whole">
-
+        
         <form action="step5.php" method="post">
 
-            <div class="checkbox"><label for="checkbox1"><input type="checkbox" checked="checked" name="checkbox" id="checkbox1"> Background skill</label></div>
-            <div class="checkbox"><label for="checkbox2"><input type="checkbox" name="checkbox" id="checkbox2"> Skill A</label></div>
-            <div class="checkbox"><label for="checkbox3"><input type="checkbox" name="checkbox" id="checkbox3"> Skill B</label></div>
-            <div class="checkbox"><label for="checkbox4"><input type="checkbox" name="checkbox" id="checkbox4" disabled> Choice Disabled</label></div>
-            <div class="checkbox has-error"><label for="checkbox5"><input type="checkbox" name="checkbox" id="checkbox5"> Choice Error</label></div>
-            <div class="checkbox has-success"><label for="checkbox6"><input type="checkbox" name="checkbox" id="checkbox6"> Choice Valid</label></div>
+            <div class="checkbox"><label for="checkbox1"><input type="checkbox" checked="checked" name="checkbox" id="checkbox1"> Background skill 1</label></div>
+            <div class="checkbox"><label for="checkbox2"><input type="checkbox" checked="checked" name="checkbox" id="checkbox2"> Background skill 2</label></div>
+            <div class="checkbox"><label for="checkbox3"><input type="checkbox" checked="checked" name="checkbox" id="checkbox3"> Background skill 3</label></div>
+            <div class="checkbox"><label for="checkbox4"><input type="checkbox" name="checkbox" id="checkbox4"> Skill A</label></div>
+            <div class="checkbox"><label for="checkbox5"><input type="checkbox" name="checkbox" id="checkbox5"> Skill B</label></div>
+            <div class="checkbox"><label for="checkbox6"><input type="checkbox" name="checkbox" id="checkbox4" disabled> Choice Disabled</label></div>
             <div class="grid one-whole">
             <p><button class="btn" type="submit">Submit</button></p>
             </div>
