ae513dae13289848aa80ba8858e7086ab8b3ded3
Ian Gormley <50596230+hj7852he@users.noreply.github.com>
2019-10-28 16:39:33 -0500

    Update step4.php

--- public/step4.php
+++ public/step4.php
@@ -1,32 +1,52 @@
 <?php require "templates/header.php"; ?>
-<div class="grid-wrapper one-whole centered">
-    <div class="grid one-whole">
+        <h3>Step 4: Skills</h3> <p>
+            Choose your skills. Skills from background are already selected.
         
-        <form action="step5.php" method="post">
+    
 
-            <div class="checkbox"><label for="checkbox1"><input type="checkbox" checked="checked" name="checkbox" id="acrobatics"> Acrobatics</label></div>
-            <div class="checkbox"><label for="checkbox2"><input type="checkbox" checked="checked" name="checkbox" id="arcana">Arcana</label></div>
-            <div class="checkbox"><label for="checkbox3"><input type="checkbox" checked="checked" name="checkbox" id="athletics">Athletics</label></div>
+        <form action="step5.php" method="post">
+        
+        <div class="grid-wrapper">
+        <div class="grid  one-third">
+          <div class="grid-wrapper">
+            <div class="grid">
+            <div class="checkbox has-success"><label for="checkbox1"><input type="checkbox" name="checkbox" id="acrobatics" checked="checked" disabled> Acrobatics</label></div>
+            <div class="checkbox"><label for="checkbox2"><input type="checkbox" name="checkbox" id="arcana">Arcana</label></div>
+            <div class="checkbox"><label for="checkbox3"><input type="checkbox" name="checkbox" id="athletics">Athletics</label></div>
             <div class="checkbox"><label for="checkbox4"><input type="checkbox" name="checkbox" id="bluff">Bluff</label></div>
             <div class="checkbox"><label for="checkbox5"><input type="checkbox" name="checkbox" id="diplomacy">Diplomacy</label></div>
             <div class="checkbox"><label for="checkbox6"><input type="checkbox" name="checkbox" id="dungeoneering"> Dungeoneering</label></div>
-            <div class="checkbox"><label for="checkbox1"><input type="checkbox" checked="checked" name="checkbox" id="endurance"> Endurance</label></div>
-            <div class="checkbox"><label for="checkbox2"><input type="checkbox" checked="checked" name="checkbox" id="heal">Heal</label></div>
-            <div class="checkbox"><label for="checkbox3"><input type="checkbox" checked="checked" name="checkbox" id="history">History</label></div>
+            </div>
+          </div>
+        </div>
+        <div class="grid  one-third">
+          <div class="grid-wrapper">
+            <div class="grid  one-third">
+            <div class="checkbox"><label for="checkbox1"><input type="checkbox" name="checkbox" id="endurance"> Endurance</label></div>
+            <div class="checkbox"><label for="checkbox2"><input type="checkbox" name="checkbox" id="heal">Heal</label></div>
+            <div class="checkbox"><label for="checkbox3"><input type="checkbox" name="checkbox" id="history">History</label></div>
             <div class="checkbox"><label for="checkbox4"><input type="checkbox" name="checkbox" id="insight">Insight</label></div>
             <div class="checkbox"><label for="checkbox5"><input type="checkbox" name="checkbox" id="intimidate">Intimidate</label></div>
             <div class="checkbox"><label for="checkbox6"><input type="checkbox" name="checkbox" id="nature"> Nature</label></div>
-            <div class="checkbox"><label for="checkbox1"><input type="checkbox" checked="checked" name="checkbox" id="perception"> Perception</label></div>
-            <div class="checkbox"><label for="checkbox2"><input type="checkbox" checked="checked" name="checkbox" id="religion">Religion</label></div>
-            <div class="checkbox"><label for="checkbox3"><input type="checkbox" checked="checked" name="checkbox" id="stealth">Stealth</label></div>
+            </div>
+          </div>
+        </div>
+            <div class="grid  one-third">
+          <div class="grid-wrapper">
+          <div class="grid  one-third">
+            <div class="checkbox"><label for="checkbox1"><input type="checkbox" name="checkbox" id="perception"> Perception</label></div>
+            <div class="checkbox"><label for="checkbox2"><input type="checkbox" name="checkbox" id="religion">Religion</label></div>
+            <div class="checkbox"><label for="checkbox3"><input type="checkbox" name="checkbox" id="stealth">Stealth</label></div>
             <div class="checkbox"><label for="checkbox4"><input type="checkbox" name="checkbox" id="streetwise">Streetwise</label></div>
             <div class="checkbox"><label for="checkbox5"><input type="checkbox" name="checkbox" id="thievery">Thievery</label></div>
+            </div>
+          </div>
+        </div>
 
-
-            <div class="grid one-whole">
+            <div class="grid">
             <p><button class="btn" type="submit">Submit</button></p>
             </div>
+            </div>
         </form>
-    </div>
-</div>
+
 <?php require "templates/footer.php"; ?>
