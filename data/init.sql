e409cf037b1b881ba8543b2fc37e69e51fc44ea0
Ben Worwa <benworwa@gmail.com>
2019-10-29 16:19:32 -0500

    fixed my database to have dummy data
    php now loads spells correctly

--- data/init.sql
+++ data/init.sql
@@ -4,59 +4,95 @@ use test;
 
 CREATE TABLE users
 (
-	id INT(11),
-	UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	id INT(11) AUTO_INCREMENT PRIMARY KEY,
 	firstname VARCHAR(30) NOT NULL,
 	lastname VARCHAR(30) NOT NULL,
 	email VARCHAR(50) NOT NULL,
 	age INT(3),
 	location VARCHAR(50),
 	date TIMESTAMP
+
 );
+INSERT INTO users (id, firstname, lastname, email, age, location, date)
+VALUES('1','test','test','test@test','23','MN','1970-01-01 00:00:01');
 
 CREATE TABLE characters
 (
 	cID int(3) NOT NULL ,
-	pID int(3) NOT NULL,
-	charactername VARCHAR(30) NOT NULL,
-	racename VARCHAR(30) NOT NULL,
+	id int(3) NOT NULL,
+	character_name VARCHAR(30) NOT NULL,
+	player_name VARCHAR(30) NOT NULL,
+	race_name VARCHAR(30) NOT NULL,
 	background VARCHAR(5000) NOT NULL,
-	proficiencies VARCHAR(500) NOT NULL,
+	hit_points INT,
+	class_name varchar(30),
+	allignment VARCHAR(30),
 	PRIMARY KEY(cID),
-	FOREIGN KEY(pID) REFERENCES users(pID)
-
+	FOREIGN KEY(id) REFERENCES users(id)
 );
-INSERT INTO users (cID, pID, character_name)
-VALUES('1', '1', 'Big Chungus')
 
+INSERT INTO characters(cID, id, character_name, player_name, race_name, background, hit_points, class_name, allignment)
+VALUES('1', '1', 'Big Chungus', 'Bigger Cspellsspellshungus', 'half elf', 'large boi', '10', 'rogue', 'baaaad boi');
 CREATE TABLE equipment
 (
-	equipmentname varchar(30) NOT NULL,
+	equipment_name varchar(30) NOT NULL,
 	cID int(3) NOT NULL,
 	equipped BOOLEAN,
-	equipmodarray int(7) NOT NULL,
-	PRIMARY KEY(equipmentname),
+	PRIMARY KEY(equipment_name),
 	FOREIGN KEY(cID) REFERENCES characters(cID)
+);
+
+CREATE TABLE armor(
+	armor_name varchar(30),
+	armorClass int,
+	PRIMARY KEY(armor_name)
+);
+
+CREATE TABLE armor_mod(
+	armor_name VARCHAR(30),
+	armor_str_mod INT,
+	armor_dex_mod INT,
+	armor_con_mod INT,
+	armor_int_mod INT,
+	armor_wis_mod INT,
+	armor_chr_mod INT,
+	FOREIGN KEY(armor_name) REFERENCES armor(armor_name)
+);
+
+CREATE TABLE weapons(
+	weapon_name VARCHAR(30),
+	PRIMARY KEY(weapon_name)
+);
+
+CREATE TABLE weapons_mod(
+	weapon_name VARCHAR(30),
+	weapons_str_mod INT,
+	weapons_dex_mod INT,
+	weapons_con_mod INT,
+	weapons_int_mod INT,
+	weapons_wis_mod INT,
+	weapons_chr_mod INT,
+	FOREIGN KEY(weapon_name) REFERENCES weapons(weapon_name)
+
 );
 
 CREATE TABLE spells
 (
-	spellname varchar(30) NOT NULL,
-	spelllevel int (1) NOT NULL,
-	spelldescription VARCHAR(500),
+	spell_name varchar(30) NOT NULL,
+	spell_level int (1) NOT NULL,
+	spell_description VARCHAR(2000),
 	cID int(3) NOT NULL,
-	PRIMARY KEY(spellname),
+	PRIMARY KEY(spell_name),
 	FOREIGN KEY(cID) REFERENCES characters(cID)
 );
-
-INSERT INTO spells (spell_name, spell_level, spell_description)
+INSERT INTO spells (spell_name, spell_level, spell_description, cID)
 VALUES ('Mage Hand', '0', 'A spectral, floating hand appears at a point you choose within range. The hand lasts for the duration or until you dismiss it as an action. The hand vanishes if it is ever more than 30 feet away from you or if you cast this spell again.
 You can use your action to control the hand. You can use the hand to manipulate an object, open an unlocked door or container, stow or retrieve an item from an open container, or pour the contents out of a vial. You can move the hand up to 30 feet each time you use it.
-The hand can’t attack, activate magic items, or carry more than 10 pounds.' )
-INSERT INTO spells (spell_name, spell_level, spell_description)
+The hand can’t attack, activate magic items, or carry more than 10 pounds.', '1' );
+INSERT INTO spells (spell_name, spell_level, spell_description, cID)
 VALUES ('Acid Splash', '0', 'You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a Dexterity saving throw or take 1d6 acid damage.
-')
-INSERT INTO spells (spell_name, spell_level, spell_description)
+', '1');
+INSERT INTO spells (spell_name, spell_level, spell_description, cID)
 VALUES('Ray of Frost', '0','A frigid beam of blue-white light streaks toward a creature within range. Make a ranged spell attack against the target. On a hit, it takes 1d8 cold damage, and its speed is reduced by 10 feet until the start of your next turn.
 ', '1');
 CREATE TABLE stats(
@@ -155,28 +191,4 @@ CREATE TABLE background_flaws(
 	background_personality VARCHAR(255),
 	personality_roll INT,
 	FOREIGN KEY(background_name) REFERENCES background(background_name)
-);
-CREATE TABLE stats(
-	cID int(3) NOT NULL,
-	strength int (3) NOT NULL,
-	dexterity int (3) NOT NULL,
-	constitution int (3) NOT NULL,
-	intelligence int (3) NOT NULL,
-	wisdom int (3) NOT NULL,
-	charisma int (3) NOT NULL,
-	FOREIGN KEY(cID) REFERENCES characters(cID)
-);
-
-CREATE TABLE race(
-	racename VARCHAR(30) NOT NULL,
-	speed VARCHAR(30) NOT NULL,
-	racestr INT(3) NOT NULL,
-	racedex INT(3) NOT NULL,
-	racecon INT(3) NOT NULL,
-	raceint INT(3) NOT NULL,
-	racewis INT(3) NOT NULL,
-	racecha INT(3) NOT NULL,
-	PRIMARY KEY(race)
- 
-
 );
