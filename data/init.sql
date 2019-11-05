3d9c794352b6a379b87bc6facfc5b5a9e08174ae
Ian Gormley <50596230+hj7852he@users.noreply.github.com>
2019-11-05 15:32:02 -0600

    insert.sql

--- data/init.sql
+++ data/init.sql
@@ -12,13 +12,13 @@ CREATE TABLE users
 
 CREATE TABLE characters
 (
-	cID int(3) NOT NULL AUTO_INCREMENT,
-	pID int(3) NOT NULL,
+	cID INT(3) NOT NULL AUTO_INCREMENT,
+	pID INT(3) NOT NULL,
 	character_name VARCHAR(30) NOT NULL,
 	player_name VARCHAR(30) NOT NULL,
 	race_name VARCHAR(30) NOT NULL,
 	background VARCHAR(5000) NOT NULL,
-	hit_points INT,
+	hit_points TINYINT,
 	class_name varchar(30),
 	allignment VARCHAR(30),
 	PRIMARY KEY(cID),
@@ -28,7 +28,7 @@ CREATE TABLE characters
 CREATE TABLE equipment
 (
 	equipment_name varchar(30) NOT NULL,
-	cID int(3) NOT NULL,
+	cID INT(3) NOT NULL,
 	equipped BOOLEAN,
 	PRIMARY KEY(equipment_name),
 	FOREIGN KEY(cID) REFERENCES characters(cID)
@@ -36,18 +36,18 @@ CREATE TABLE equipment
 
 CREATE TABLE armor(
 	armor_name varchar(30),
-	armorClass int,
+	armorClass TINYINT,
 	PRIMARY KEY(armor_name)
 );
 
 CREATE TABLE armor_mod(
 	armor_name VARCHAR(30),
-	armor_str_mod INT,
-	armor_dex_mod INT,
-	armor_con_mod INT,
-	armor_int_mod INT,
-	armor_wis_mod INT,
-	armor_chr_mod INT,
+	armor_str_mod TINYINT,
+	armor_dex_mod TINYINT,
+	armor_con_mod TINYINT,
+	armor_int_mod TINYINT,
+	armor_wis_mod TINYINT,
+	armor_chr_mod TINYINT,
 	FOREIGN KEY(armor_name) REFERENCES armor(armor_name)
 );
 
@@ -58,12 +58,12 @@ CREATE TABLE weapons(
 
 CREATE TABLE weapons_mod(
 	weapon_name VARCHAR(30),
-	weapons_str_mod INT,
-	weapons_dex_mod INT,
-	weapons_con_mod INT,
-	weapons_int_mod INT,
-	weapons_wis_mod INT,
-	weapons_chr_mod INT,
+	weapons_str_mod TINYINT,
+	weapons_dex_mod TINYINT,
+	weapons_con_mod TINYINT,
+	weapons_int_mod TINYINT,
+	weapons_wis_mod TINYINT,
+	weapons_chr_mod TINYINT,
 	FOREIGN KEY(weapon_name) REFERENCES weapons(weapon_name)
 
 );
@@ -71,21 +71,21 @@ CREATE TABLE weapons_mod(
 CREATE TABLE spells
 (
 	spell_name varchar(30) NOT NULL,
-	spell_level int (1) NOT NULL,
+	spell_level TINYINT (1) NOT NULL,
 	spell_description VARCHAR(500),
-	cID int(3) NOT NULL,
+	cID INT(3) NOT NULL,
 	PRIMARY KEY(spell_name),
 	FOREIGN KEY(cID) REFERENCES characters(cID)
 );
 
 CREATE TABLE stats(
-	cID int(3) NOT NULL,
-	strength int (3) NOT NULL,
-	dexterity int (3) NOT NULL,
-	constitution int (3) NOT NULL,
-	intelligence int (3) NOT NULL,
-	wisdom int (3) NOT NULL,
-	charisma int (3) NOT NULL,
+	cID INT(3) NOT NULL,
+	strength TINYINT (3) NOT NULL,
+	dexterity TINYINT (3) NOT NULL,
+	constitution TINYINT (3) NOT NULL,
+	intelligence TINYINT (3) NOT NULL,
+	wisdom TINYINT (3) NOT NULL,
+	charisma TINYINT (3) NOT NULL,
 	FOREIGN KEY(cID) REFERENCES characters(cID)
 );
 
@@ -98,19 +98,19 @@ CREATE TABLE race(
 
 CREATE TABLE raceMod(
 	race_name VARCHAR(30),
-	race_str_mod INT,
-	race_dex_mod INT,
-	race_con_mod INT,
-	race_int_mod INT,
-	race_wis_mod INT,
-	race_chr_mod INT,
+	race_str_mod TINYINT,
+	race_dex_mod TINYINT,
+	race_con_mod TINYINT,
+	race_int_mod TINYINT,
+	race_wis_mod TINYINT,
+	race_chr_mod TINYINT,
 	FOREIGN KEY(race_name) REFERENCES race(race_name)
 );
 
 CREATE TABLE class_name(
 	class_name VARCHAR(30),
-	proficiency_bonus INT,
-	hit_dice INT,
+	proficiency_bonus TINYINT,
+	hit_dice TINYINT,
 	PRIMARY KEY(class_name)
 );
 
@@ -127,14 +127,14 @@ CREATE TABLE class_equipment(
 );
 
 CREATE TABLE skills(
-	cID int,
+	cID INT,
 	skill_name varchar(30),
 	FOREIGN KEY(cID) REFERENCES characters(cID)
 );
 
 CREATE TABLE background(
 	background_name VARCHAR(30),
-	skill_proficiency_1 varchar(30),
+	skill_proficiency_1 VARCHAR(30),
 	skill_proficiency_2 VARCHAR(30),
 	PRIMARY KEY(background_name)
 
@@ -149,33 +149,33 @@ CREATE TABLE background_equipment(
 CREATE TABLE background_personality(
 	background_name VARCHAR(30),
 	background_personality VARCHAR(255),
-	personality_roll INT,
+	personality_roll TINYINT,
 	FOREIGN KEY(background_name) REFERENCES background(background_name)
 );
 
 CREATE TABLE background_bonds(
 	background_name VARCHAR(30),
 	background_bond VARCHAR(255),
-	bonds_roll INT,
+	bonds_roll TINYINT,
 	FOREIGN KEY(background_name) REFERENCES background(background_name)
 );
 
 CREATE TABLE background_ideals(
 	background_name VARCHAR(30),
 	background_ideal VARCHAR(255),
-	personality_roll INT,
+	personality_roll TINYINT,
 	FOREIGN KEY(background_name) REFERENCES background(background_name)
 );
 
 CREATE TABLE background_flaws(
 	background_name VARCHAR(30),
 	background_flaw VARCHAR(255),
-	personality_roll INT,
+	personality_roll TINYINT,
 	FOREIGN KEY(background_name) REFERENCES background(background_name)
 );
 
 CREATE TABLE characteristics(
-	cID int,
+	cID INT,
 	personality VARCHAR(255),
 	ideal VARCHAR(255),
 	bond VARCHAR(255),
---
+++ data/insert.sql
@@ -0,0 +1,12 @@
+USE dnd;
+
+INSERT INTO background
+VALUES
+  ('Acolyte', 'Insight', 'Religion'),
+  ('Charlaton', 'Deception', 'Sleight of Hand'),
+  ('Criminal', 'Deception', 'Stealth'),
+  ('Entertainer', 'Acrobatics', 'Performance'),
+  ('Folk Hero', 'Animal Handling', 'Survival'),
+  ('Guild Artisan', 'Insight', 'Persuasion'),
+  ('Hermit', 'Medicine', 'Religion'),
+  ('Noble', 'History', 'Persuasion'),;
