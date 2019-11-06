a1f91a402d399b39b33fe5074f0c77e5b44fce81
Ben Worwa <benworwa@gmail.com>
2019-11-06 13:50:10 -0600

    IT'S ALIVE

--- public/step8.php
+++ public/step8.php
@@ -4,22 +4,21 @@
 
 
  session_start();
- $spellvar = $_POST['spell_select'];
+ $equipmentvar = $_POST['equipment_select'];
 
  $con=mysqli_connect("localhost","root","root","dnd");
  // Check connection
  if (mysqli_connect_errno()){
    echo "Failed to connect to MySQL: " . mysqli_connect_error();
  }
- if(isset($_POST['spell_submit'])){
+ if(isset($_POST['equipment_submit'])){
  // Perform query 
 
-   $sql = "UPDATE spells SET equipped = 1 WHERE spell_name = '$spellvar'";
-  $sql1 = "INSERT INTO spells ( spell_name, spell_level, spell_description, cID, equipped) VALUES ( test1 , 0 , test1 , 1 , 0 )";
+   $sql = "UPDATE equipment SET equipped = 1 WHERE equipment_name = '$equipmentvar'";
    mysqli_query($con, $sql);
 
    
-  header('Location: step8.php');
+  header('Location: display.php');
  }
 
 ?>
@@ -27,30 +26,27 @@
 <?php
 
     $mysqli = NEW MySQLi('localhost','root','root','dnd');
-    $resultSet = $mysqli->query("SELECT spell_name FROM spells")
+    $resultSet = $mysqli->query("SELECT equipment_name FROM equipment")
     
      ?>
 
 <form action="" method="post">
-    <label for="spell_select"> Select a Spell</label>
+    <label for="equipment_select"> Select an equipment</label>
 
-    <select name="spell_select" class="form-control">
+    <select name="equipment_select" class="form-control">
       <?php
       while($rows = $resultSet->fetch_assoc())
       {
-        $spell_name = $rows['spell_name'];
-        echo "<option value='$spell_name'>$spell_name</option>";
+        $equipment_name = $rows['equipment_name'];
+        echo "<option value='$equipment_name'>$equipment_name</option>";
       }
        ?>
     </select>
     
               
-    <input type="submit" value="Submit" name="spell_submit" id="spell_submit">
+    <input type="submit" value="Submit" name="equipment_submit" id="equipment_submit">
   
 </form>
-<?php
-$_SESSION['test'] = $_POST['test_select'];
-?>
 </div>
 <div class="grid-lap one-third">
 <div class="grid-lap one-third">
