7c14ef7512903a2365b70c6b0303e600f3b43cf6
Nathan <nate.gjerd@icloud.com>
2019-11-23 16:46:18 -0600

    added carousel to step 1.php

--- public/step0.php
+++ public/step0.php
@@ -1,8 +1,6 @@
 <?php include "templates/header.php"; ?>
 <?php include "templates/session.php"; ?>
 <?php
-
-  
   $con=mysqli_connect("localhost","root","root","dnd");
   // Check connection
   if (mysqli_connect_errno()){
@@ -20,7 +18,6 @@
     $_SESSION['cid'] = $last_id;
     header('Location: step1.php');
   }
-
 ?>
 
 
@@ -96,6 +93,35 @@
   <input type="submit" value="Submit" name="char_submit" id="char_submit">
 
   </form>
+</div>
+
+<div class="our-first-carousel">
+  <div class="site-wrapper">
+    <div class="grid-wrapper centered">
+      <div class="grid">
+        <div class="carousel-wrapper">
+          <span id="item-1"></span>
+          <span id="item-2"></span>
+          <span id="item-3"></span>
+          <div class="carousel-item item-1 text-center">
+            <h2>Title of Character 1</h2>
+            <a class="arrow arrow-prev" href="#item-3"></a>
+            <a class="arrow arrow-next" href="#item-2"></a>
+          </div>
+          <div class="carousel-item item-2 text-center">
+            <h2>Title of Character 2</h2>
+            <a class="arrow arrow-prev" href="#item-1"></a>
+            <a class="arrow arrow-next" href="#item-3"></a>
+          </div>
+          <div class="carousel-item item-3 text-center">
+            <h2>Title of Character 3</h2>
+            <a class="arrow arrow-prev" href="#item-2"></a>
+            <a class="arrow arrow-next" href="#item-1"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 
--- public/templates/header.php
+++ public/templates/header.php
@@ -5,9 +5,10 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
 	<title>DnD Character Creation Tool</title>
 	<link rel="stylesheet" href="css/application.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/tiny-slider.css">
+	<!--[if (lt IE 9)]><script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.helper.ie8.js"></script><![endif]-->
 </head>
 
 <body>
