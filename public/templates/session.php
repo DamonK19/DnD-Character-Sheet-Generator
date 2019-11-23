20c82d0b776cca33752a4007f6f735f670af4041
Damon <damonkasner@gmail.com>
2019-11-23 12:15:06 -0600

    Basic logout and session clear

--- public/templates/session.php
+++ public/templates/session.php
@@ -2,3 +2,17 @@
 session_start();
  echo "Current User: ", $_SESSION['email'];
 ?>
+
+<?php
+if(isset($_POST['logout'])){
+  session_unset();
+  session_destroy();
+  header('Location: index.php');
+}
+ ?>
+
+<div>
+  <form method="post">
+    <input type="submit" value="Logout" name="logout" id="logout">
+  </form>
+</div>
