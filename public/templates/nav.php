e82cbf38ad3ba1bd91e72194622e768183e5f3ad
Ian Gormley <50596230+hj7852he@users.noreply.github.com>
2019-12-04 21:10:31 -0600

    fix logout

--- public/templates/nav.php
+++ public/templates/nav.php
@@ -1,5 +1,6 @@
 <!-- background -->
 <div class="header-bg">
+    <div class="site-wrapper">
     <div class="grid-wrapper">
         <div class="grid one-fifth text-center site-logo">
             <a href="manageCharacter.php"><img src="assets/images/dd.png" alt=""></a>
@@ -9,26 +10,25 @@
         </div>
         <div class="grid one-fifth user-content">
             <div class="grid-wrapper user-info align--center">
-                <div class="grid user-name">
+                <div class="grid one-half user-name">
                     <a href="manageCharacter.php">
-                        <p class="black"><?php session_start();
-                                            echo $_SESSION['email']; ?></p>
+                        <p class="black"><?php session_start(); echo $_SESSION['email']; ?></p>
                     </a>
-
                 </div>
+                        <div class="grid one-half">
+                            <?php
+                            if (isset($_POST['logout'])) {
+                                session_unset();
+                                session_destroy();
+                                header('Location: index.php');
+                            }
+                            ?>
+                            <form method="post">
+                                <input type="submit" value="Logout" name="logout" id="logout">
+                            </form>
+                        </div>
             </div>
         </div>
-        <div>
-            <?php
-            if (isset($_POST['logout'])) {
-                session_unset();
-                session_destroy();
-                header('Location: index.php');
-            }
-            ?>
-            <form method="post">
-                <input type="submit" value="Logout" name="logout" id="logout">
-            </form>
         </div>
     </div>
 </div>
