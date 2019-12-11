d91a335e3eca97c7959ea84e7990f043af2e3d76
Damon <damonkasner@gmail.com>
2019-12-11 17:12:38 -0600

    can't go to other pages if not logged in

--- public/templates/nav.php
+++ public/templates/nav.php
@@ -17,6 +17,11 @@
                 </div>
                         <div class="grid one-half">
                             <?php
+
+                            if(!isset($_SESSION['id'])){
+                              header('Location: index.php');
+                            }
+
                             if (isset($_POST['logout'])) {
                                 session_unset();
                                 session_destroy();
